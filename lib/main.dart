b0e96d67f704de3d747f847f55d3ae85048e4248
IsakBH <isak@brunhenriksen.net>
2026-07-19 20:17:54 +0200

    lærer stateful widgets og har brukt litt setstate og sånn. jeg skjønner ikke så kjempe mye akkurat nå, men det går fint. finner ut av det.

--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,8 @@ void main() {
   runApp(const MyApp());
 }
 
+String title = 'Isak Henriksen';
+
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
@@ -17,20 +19,37 @@ class MyApp extends StatelessWidget {
           brightness: Brightness.dark,
         ),
       ),
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('Isak Henriksen'), 
-          centerTitle: true,
-        ),
+      home: MyHomePage(),
+    );
+  }
+}
 
-        bottomNavigationBar: NavigationBar(
-          destinations: [
-            NavigationDestination(icon: Icon(Icons.home), label: "Home"),
-            NavigationDestination(icon: Icon(Icons.tune), label: "Tune"),
-          ],
-          selectedIndex: 0,
-          onDestinationSelected: (int value) {},
-        ),
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key? key}) : super(key: key);
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int current_index = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('Isak Henriksen'), centerTitle: true),
+
+      bottomNavigationBar: NavigationBar(
+        destinations: [
+          NavigationDestination(icon: Icon(Icons.home), label: "Home"),
+          NavigationDestination(icon: Icon(Icons.person), label: "Profile"),
+        ],
+        selectedIndex: current_index,
+        onDestinationSelected: (int value) {
+          setState(() {
+            current_index = value;
+          });
+        },
       ),
     );
   }
