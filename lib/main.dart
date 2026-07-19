8c2cae8bc619c499b1d26104a26282984c6e4a7e
IsakBH <isak@brunhenriksen.net>
2026-07-19 20:38:29 +0200

    fjernet alt og startet helt på nytt igjen. lagt tilbake navigation bar og appbar, som er kult. nå er alt sånn stateful widget. stilig.

--- lib/main.dart
+++ lib/main.dart
@@ -4,11 +4,14 @@ void main() {
   runApp(const MyApp());
 }
 
-String title = 'Isak Henriksen';
-
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({Key? key}) : super(key: key);
 
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -17,41 +20,19 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(
           seedColor: Colors.teal,
           brightness: Brightness.dark,
-        ),
+          ),
       ),
-      home: MyHomePage(),
-    );
-  }
-}
 
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key? key}) : super(key: key);
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("Isak Henriksen"),
+          centerTitle: true,
+        ),
 
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int current_index = 0;
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(title: Text('Isak Henriksen'), centerTitle: true),
-
-      body: current_index == 0 ? Center(child: Text("Hello"),) : Center(child: Text("Goodbye"),),
-
-      bottomNavigationBar: NavigationBar(
-        destinations: [
+        bottomNavigationBar: NavigationBar(destinations: [
           NavigationDestination(icon: Icon(Icons.home), label: "Home"),
           NavigationDestination(icon: Icon(Icons.person), label: "Profile"),
-        ],
-        selectedIndex: current_index,
-        onDestinationSelected: (int value) {
-          setState(() {
-            current_index = value;
-          });
-        },
+        ]),
       ),
     );
   }
