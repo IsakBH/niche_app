79e8ba96a0d3ff96730844c3d11a67702fdc73d2
isakbh <isak@brunhenriksen.net>
2026-07-25 23:22:53 +0200

    toggling av dark og light mode fungerer nå! jeg wrappet hele MaterialApp() inn i en ValueListenableBuilder(), og inni der brukte jeg en ternary operator for å bytte brightnessen inni ThemeData() til Brightness.dark og Brightness.light

--- lib/main.dart
+++ lib/main.dart
@@ -16,19 +16,21 @@ class MyApp extends StatefulWidget {
 class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(
-          seedColor: Colors.teal,
-          brightness: Brightness.light,
+    return ValueListenableBuilder(
+      valueListenable: isDarkModeNotifier,
+      builder: (BuildContext context, dynamic isDarkMode, Widget? child) {
+        return MaterialApp(
+          debugShowCheckedModeBanner: false,
+          theme: ThemeData(
+            colorScheme: ColorScheme.fromSeed(
+              seedColor: Colors.teal,
+              brightness: isDarkMode ? Brightness.dark : Brightness.light,
+            ),
+          ),
 
-
-
-        ),
-      ),
-
-      home: WidgetTree(),
+          home: WidgetTree(),
+        );
+      },
     );
   }
 }
