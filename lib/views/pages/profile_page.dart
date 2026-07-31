30b01011e1eb42acf5bd28ff91cd502426ed7471
isakbh <isak@brunhenriksen.net>
2026-08-01 00:04:44 +0200

    endret dark mode til default, og lagt til en TextField, en CheckBox() og en CheckBoxListTile() i profile_page.dart. Det er ganske stilig! Tror jeg skjønner alt, men jeg er ikke helt sikker.

--- lib/views/pages/profile_page.dart
+++ lib/views/pages/profile_page.dart
@@ -1,10 +1,51 @@
 import 'package:flutter/material.dart';
 
-class ProfilePage extends StatelessWidget {
+class ProfilePage extends StatefulWidget {
   const ProfilePage({super.key});
 
   @override
+  State<ProfilePage> createState() => _ProfilePageState();
+}
+
+class _ProfilePageState extends State<ProfilePage> {
+  TextEditingController controller = TextEditingController();
+  bool? isChecked = false;
+  @override
   Widget build(BuildContext context) {
-    return Center(child: Text("Profilside"));
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: Column(
+        children: [
+          TextField(
+            controller: controller,
+            decoration: InputDecoration(border: OutlineInputBorder()),
+            onChanged: (value) {
+              setState(() {});
+            },
+          ),
+          Text(controller.text),
+
+          Checkbox(
+            tristate: true,
+            value: isChecked,
+            onChanged: (bool? value) {
+              setState(() {
+                isChecked = value;
+              });
+            },
+          ),
+          CheckboxListTile(
+            tristate: true,
+            value: isChecked,
+            title: Text("Click me"),
+            onChanged: (bool? value) {
+              setState(() {
+                isChecked = value;
+              });
+            },
+          ),
+        ],
+      ),
+    );
   }
 }
